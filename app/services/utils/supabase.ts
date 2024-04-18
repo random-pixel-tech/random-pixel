a802110ae8becece91c2a0aaad98eefcf3177c3b
Anukrati Mehta <anukratimehta@gmail.com>
2024-04-18 14:39:56 +0530

    Add supabase local

--- app/services/utils/supabase.ts
+++ app/services/utils/supabase.ts
@@ -2,8 +2,14 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = "https://skaqfhynzijluwabysio.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrYXFmaHluemlqbHV3YWJ5c2lvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0Mzk1ODksImV4cCI6MjAyNjAxNTU4OX0.oQe6xlAJJNGV6rAB8-6zToAy-g6DvPlKNO2oKi2K8B0";
+// export const supabaseUrl = "https://skaqfhynzijluwabysio.supabase.co";
+
+// Update the Supabase URL to your local instance URL
+export const supabaseUrl = 'http://127.0.0.1:54321';
+
+// const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrYXFmaHluemlqbHV3YWJ5c2lvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0Mzk1ODksImV4cCI6MjAyNjAxNTU4OX0.oQe6xlAJJNGV6rAB8-6zToAy-g6DvPlKNO2oKi2K8B0";
+
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
