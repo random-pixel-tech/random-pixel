e7177e4ac828e281dc621d6b613ad1b70e07d747
Anukrati Mehta <anukratimehta@gmail.com>
2024-04-20 15:56:35 +0530

    Fix Month Format

--- app/components/DatePicker.tsx
+++ app/components/DatePicker.tsx
@@ -23,7 +23,7 @@ const DatePicker: React.FC<DatePickerProps> = ({ isOpen, handleDatePickerCancel,
     };
 
     const isValidMonth = (month: string) => {
-        const validMonths = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+        const validMonths = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
         return validMonths.includes(month);
     };
 
@@ -64,7 +64,7 @@ const DatePicker: React.FC<DatePickerProps> = ({ isOpen, handleDatePickerCancel,
             </Box>
             <Box display="flex" flexDirection='column' w="$1/5">
                 <Input variant="underlined" isInvalid={!isValidMonth(startMonth)}>
-                    <InputField w="$0.5" placeholder='JAN' value={startMonth} onChangeText={handleStartMonthChange}>
+                    <InputField w="$0.5" placeholder='04' value={startMonth} onChangeText={handleStartMonthChange}>
                     </InputField>
                 </Input>
                 <Text alignSelf='center'>
