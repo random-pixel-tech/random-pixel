7b481ffd1d0e1f8797fb412097a47e4ab2be9a1f
Anukrati Mehta <anukratimehta@gmail.com>
2024-05-06 16:52:25 +0530

    Update useStudentAttendance

--- app/screens/HomePage.tsx
+++ app/screens/HomePage.tsx
@@ -1,21 +1,40 @@
 import React, { useEffect } from 'react';
-import { Box, ScrollView, Text } from '@gluestack-ui/themed';
-import { supabase } from '../services/utils/supabase';
-import ClassAttendanceCard from '../components/ClassAttendanceCard';
-import { useAttendanceStats } from '../services/utils/api/useAttendanceStats';
+import { Box, Text } from '@gluestack-ui/themed';
+import useStudentAttendance from '../services/utils/api/useStudentAttendance';
 
 const Home: React.FC = () => {
-  // const { classData } = useAttendanceStats();
+  const {
+    studentAttendanceData,
+    className,
+    section,
+    today,
+    fetchAllStudentAttendance,
+  } = useStudentAttendance();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch and log student attendance data for the current class
+        console.log('Student Attendance Data:', studentAttendanceData);
+
+        // Fetch and log all student attendance data
+        const allStudentAttendanceData = await fetchAllStudentAttendance();
+        console.log('All Student Attendance Data:', allStudentAttendanceData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [studentAttendanceData, fetchAllStudentAttendance]);
 
   return (
     <Box h="100%">
       <Text>Home Screen</Text>
-      {/* <ScrollView>
-      {classData.map((data) => (
-        <ClassAttendanceCard key={data.classId} classData={data} />
-      ))}
-      </ScrollView> */}
-          </Box>
+      <Text>Class: {className}</Text>
+      <Text>Section: {section}</Text>
+      <Text>Date: {today}</Text>
+    </Box>
   );
 };
 
