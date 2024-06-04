ea7b6ed6182277b6e02abb2a683b2bbc25394d9f
Anukrati Mehta <anukratimehta@gmail.com>
2024-06-04 16:47:55 +0530

    Add select components

--- app/components/SelectDropdown.tsx
+++ app/components/SelectDropdown.tsx
@@ -1,2 +1,43 @@
-import { Select } from '@gluestack-ui/themed';
+import React, { useState } from 'react';
+import { Text, VStack } from '@gluestack-ui/themed';
 
+interface DropdownOption {
+  label: string;
+  value: string;
+}
+
+const SelectDropdown: React.FC<{
+  label: string;
+  options: DropdownOption[];
+  defaultValue?: string;
+  onChange?: (value: string) => void;
+}> = ({ label, options, defaultValue, onChange }) => {
+  const [selectedValue, setSelectedValue] = useState<string>(defaultValue || '');
+
+  const handleValueChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    setSelectedValue(value);
+    if (onChange) {
+      onChange(value);
+    }
+  };
+
+  return (
+    <VStack>
+      <Text mb="$2">{label}</Text>
+      <select
+        value={selectedValue}
+        onChange={handleValueChange}
+        style={{ width: 'full', backgroundColor: '##9F83EB' }}
+      >
+        {options.map(({ label, value }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </VStack>
+  );
+};
+
+export default SelectDropdown;
--- app/screens/ControlRoomScreen/ReportGeneration/GenerateReport.tsx
+++ app/screens/ControlRoomScreen/ReportGeneration/GenerateReport.tsx
@@ -1,14 +1,51 @@
-import React from 'react';
-import { Box } from '@gluestack-ui/themed';
+import React, { useState } from 'react';
+import { Box, VStack } from '@gluestack-ui/themed';
 import Header from '../../../components/Header';
+import SelectDropdown from '../../../components/SelectDropdown';
 
 const GenerateReport = () => {
+  const [selectedDuration, setSelectedDuration] = useState('');
+  const [selectedClass, setSelectedClass] = useState('');
+
+  const durationOptions = [
+    { label: 'Jan (2024)', value: 'jan2024' },
+    { label: 'Feb (2024)', value: 'feb2024' },
+    { label: 'Mar (2024)', value: 'mar2024' },
+    { label: 'Dec (2024)', value: 'dec2024' },
+  ];
+
+  const classOptions = [
+    { label: '1 A', value: '1a' },
+    { label: '1 B', value: '1b' },
+  ];
+
+  const handleDurationChange = (value: string) => {
+    setSelectedDuration(value);
+    // Perform any additional operations with the selected duration
+  };
+
+  const handleClassChange = (value: string) => {
+    setSelectedClass(value);
+    // Perform any additional operations with the selected class
+  };
 
   return (
     <Box bg="$pixSecondaryLight50" w="$full" h="$full">
-      <Header
-        title="Generate Attendance Report"
+      <Header title="Generate Attendance Report" />
+      <VStack mt="$4" px="$4">
+        <SelectDropdown
+          label="Select Duration"
+          options={durationOptions}
+          defaultValue={durationOptions[0].value}
+          onChange={handleDurationChange}
         />
+        <SelectDropdown
+          label="Select Class"
+          options={classOptions}
+          defaultValue={classOptions[0].value}
+          onChange={handleClassChange}
+        />
+      </VStack>
     </Box>
   );
 };
